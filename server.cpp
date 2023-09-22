30401a8ee83a625542926d0b8c757690570cb682
Nicoara Andrei Costin <64546774+diuand@users.noreply.github.com>
2023-09-22 15:37:15 +0300

    Add files via upload
    working chat between clients

--- server.cpp
+++ server.cpp
@@ -5,73 +5,98 @@
 #include <vector>
 #include <deque>
 using namespace boost::asio;
+
 const int MAX_TEXT_LENGTH = 1000;
-int client_id = 0;
-
-std::vector<ip::tcp::socket> clients;
 
 
-void read_from_client(ip::tcp::socket& socket, int client_id) {
-    boost::asio::streambuf buffer;
+void read_data(ip::tcp::socket& socket)
+{
+    // Create a buffer
     std::array<char, MAX_TEXT_LENGTH> read_msg;
 
-    while (true) {
-        boost::system::error_code ec;
-        std::size_t bytes_transferred = socket.read_some(boost::asio::buffer(read_msg), ec);
+    // Async read
+    socket.async_read_some(boost::asio::buffer(read_msg),
+        [&](boost::system::error_code ec, std::size_t bytes_transferred) {
+            if (!ec) {
+                // Display the received message
+                std::cout.write(read_msg.data(), bytes_transferred);
+                std::cout << "\n";
+                read_data(socket); // Continue reading
+            }
+            else {
+                std::cerr << "Read error: " << ec.message() << "\n";
+            }
+        });
+}
 
-        if (!ec) {
-            std::cout << "Client "<< client_id <<": ";
-            std::cout.write(read_msg.data(), bytes_transferred);
-            std::cout << "\n";
 
-        }
-        else {
-            std::cout << "Client " << client_id << " disconnected " << std::endl;
-            break;  
-        }
+//list of messages
+std::deque<std::string> messages;
+
+
+// the actual sending funcion to the server
+void write_data(ip::tcp::socket& socket, io_service& ioservice) {
+    if (!messages.empty()) {
+        //send data async
+        async_write(socket, buffer(messages.front().data(), messages.front().length()), [&](boost::system::error_code ec, std::size_t) {
+            if (!ec) {
+                //clean after sening
+                messages.pop_front();
+                //repeat for more messages
+                if (!messages.empty()) {
+                    write_data(socket, ioservice);
+                }
+            }
+            });
     }
 }
 
-void handle_client(ip::tcp::socket socket) {
-    std::cout << "creating session on: "
-        << socket.remote_endpoint().address().to_string()
-        << ":" << socket.remote_endpoint().port() << '\n';
-    client_id++;
-    clients.push_back(std::move(socket));
-    read_from_client(clients.back(), client_id);
+
+//verify if it is a message
+void send_message(ip::tcp::socket& socket, std::string message, io_service& ioservice) {
+    messages.push_back(message);
+    if (!message.empty()) {
+        write_data(socket, ioservice);
+
+    }
+
+
+
+
+
 }
 
 
-
-
-
-void do_accept(io_service& ioservice, ip::tcp::acceptor& acceptor) {
-    acceptor.async_accept([&](boost::system::error_code ec, ip::tcp::socket socket) {
-        if (!ec) {
-            std::thread(handle_client, std::move(socket)).detach();
-            
-        }
-        else {
-            std::cerr << "Error: " << ec.message() << std::endl;
-        }
-        do_accept(ioservice, acceptor);
-        });
-
-}
 
 int main() {
     try {
         // Create a pipe to the server
         io_service ioservice;
+        ip::tcp::socket socket(ioservice);
+        ip::tcp::resolver resolver(ioservice);
 
-        ip::tcp::endpoint endpoint(ip::tcp::v4(), 55004);
+        // read data after connecting
+        async_connect(socket, resolver.resolve({ "127.0.0.1", "55004" }), [&](boost::system::error_code ec, ip::tcp::endpoint) {
+            if (!ec) {
+                read_data(socket);
+            }
+            else {
+                std::cerr << "Exception: " << ec.message() << "\n";
+            }
+            });
 
-        ip::tcp::acceptor acceptor(ioservice, endpoint);
+        //ioservice thread
+        std::thread t([&ioservice]() { ioservice.run(); }); // the thread is runnging just on the t.join
 
-        do_accept(ioservice, acceptor);
-        ioservice.run();
-        std::cout << "aici";
+        //writing loop
+        char line[MAX_TEXT_LENGTH];
+        while (std::cin.getline(line, MAX_TEXT_LENGTH + 1)) {
+            std::string message(line);
+            send_message(socket, message, ioservice);
 
+        }
+
+        t.join(); // Wait for the thread to finish
     }
     catch (std::exception& e) {
         std::cerr << "Exception: " << e.what() << std::endl;
