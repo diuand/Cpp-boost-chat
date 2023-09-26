88583a275bcef54e1a37acf9336493e29c2a895a
Nicoara Andrei Costin <64546774+diuand@users.noreply.github.com>
2023-09-26 11:15:43 +0300

    Add files via upload
    chat history implementation

--- server.cpp
+++ server.cpp
@@ -9,13 +9,35 @@ using namespace boost::asio;
 
 
 const int MAX_TEXT_LENGTH = 1000; //maximum text length
-int client_id = 0; // for verbosity
+
+// create a message history for a new client
+const int MAX_HISTORY_MESSAGES = 100;
+std::deque<std::string> history_messages;
+
+
+//client details
+struct Client_data {
+    std::shared_ptr<ip::tcp::socket> socket;
+    int id;
+};
 
 //list of clients
-std::vector<std::shared_ptr<ip::tcp::socket>> clients;
+std::vector<Client_data> clients;
+
+
+void write_history(Client_data client) {
+
+    for (const std::string& history_message : history_messages) {
+        // Send the message length followed by the message itself
+       /* std::size_t message_length = history_message.size();
+        boost::asio::write(*client.socket, boost::asio::buffer(&message_length, sizeof(message_length)));*/
+        boost::asio::write(*client.socket, boost::asio::buffer(history_message+'\n'));
+    }
+}
+
 
 //read from client and write to all clients
-void read_from_client(std::shared_ptr<ip::tcp::socket>  socket, int client_id) {
+void read_from_client(Client_data client) {
     //read message buffer
     boost::asio::streambuf buffer;
     std::array<char, MAX_TEXT_LENGTH> read_msg;
@@ -26,26 +48,37 @@ void read_from_client(std::shared_ptr<ip::tcp::socket>  socket, int client_id) {
         boost::system::error_code ec;
 
         //the actual read
-        std::size_t bytes_transferred = socket->read_some(boost::asio::buffer(read_msg), ec);
+        std::size_t bytes_transferred = client.socket->read_some(boost::asio::buffer(read_msg), ec);
 
         if (!ec) {
             //display the client data
-            std::cout << "Client "<< client_id <<": ";
+            std::cout << "Client "<< client.id <<": ";
             std::cout.write(read_msg.data(), bytes_transferred);
             std::cout << "\n";
 
             //create a message to be sended to all clients
-            std::string message = "Client " + std::to_string(client_id) + ": ";
+            std::string message = "Client " + std::to_string(client.id) + ": ";
             message.append(read_msg.data(), bytes_transferred);
+
+            history_messages.push_back(message);// append the message history
+
+
+            // Limit the number of historical messages
+            if (history_messages.size() > MAX_HISTORY_MESSAGES) {
+                history_messages.pop_front();
+            }
+
 
             //sending the message to all clients
             for (auto& client : clients) {
-                boost::asio::write(*client, boost::asio::buffer(message));
+                boost::asio::write(*client.socket, boost::asio::buffer(message));
             }
         }
         else { // error ocurred on reading form clients or disconnecting client from the server
-            std::cout << "Client " << client_id << " disconnected " << std::endl;
-            clients.erase(std::remove(clients.begin(), clients.end(), socket), clients.end());
+            std::cout << "Client " << client.id << " disconnected " << std::endl;
+            //remove the client after disconnecting
+            clients.erase(std::remove_if(clients.begin(), clients.end(),
+                [&](const Client_data& c) { return c.socket == client.socket; }), clients.end());
             break;  
         }
     }
@@ -54,16 +87,21 @@ void read_from_client(std::shared_ptr<ip::tcp::socket>  socket, int client_id) {
 //intermediary function for client handling
 void handle_client(std::shared_ptr<ip::tcp::socket> socket) {
 
+    static int id{ 0 };// for verbosity
+
     //display client details after connection
-    std::cout << "creating session on: "
-        << socket->remote_endpoint().address().to_string()
-        << ":" << socket->remote_endpoint().port() << '\n';
+    std::cout << "Client "<< ++id <<" connected to port " << socket->remote_endpoint().port() << "\n";
     
-    client_id++; // for verbosity
-   
-    clients.push_back(socket);  //insert the new client
     
-    read_from_client(socket, client_id); //read data from client and write data to all clients
+    clients.push_back(Client_data{ socket,id });  //insert the new client
+    if (history_messages.size() == 0) {
+        read_from_client(clients.back()); //read data from client and write data to all clients
+    }
+    else {
+        write_history(clients.back());
+        read_from_client(clients.back()); //read data from client and write data to all clients
+    }
+    
 }
 
 //accept the new client and start a separate thread for him
